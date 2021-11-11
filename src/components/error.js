0b7ae7bc1e1160afebe338df6ef13e1ee96efaba
Aman Manhas <aman.manhas@tothenew.com>
2021-11-11 11:18:18 +0530

    Error color changed to red

--- src/components/error.js
+++ src/components/error.js
@@ -8,7 +8,7 @@ function ErrorToast(props) {
   return (
     <BaseToast
       {...props}
-      style={{ borderLeftColor: colors.blazeOrange }}
+      style={{ borderLeftColor: colors.redColor }}
       leadingIcon={icons.error}
     />
   );
